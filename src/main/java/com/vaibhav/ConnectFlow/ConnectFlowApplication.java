8c99e325ccf76eaa941bbec382bfc8fcd0fe0bab
Vaibhav Kumar Chaurasiya <vaibhavkumarchaurasiya5881@gmail.com>
2025-08-13 16:25:32 +0530

    Add HTML and CSS files for application UI

--- src/main/java/com/vaibhav/ConnectFlow/ConnectFlowApplication.java
+++ src/main/java/com/vaibhav/ConnectFlow/ConnectFlowApplication.java
@@ -1,13 +1,40 @@
 package com.vaibhav.ConnectFlow;
 
+import com.vaibhav.ConnectFlow.user.User;
+import com.vaibhav.ConnectFlow.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConnectFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectFlowApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner (UserService userService) {
+		return args -> {
+			userService.register(User.builder()
+							.username("Vaibhav")
+							.email("vaibhav@gmail.com")
+							.password("vvv")
+					.build());
+
+			userService.register(User.builder()
+					.username("Abhay")
+					.email("abhay@gmail.com")
+					.password("aaa")
+					.build());
+
+			userService.register(User.builder()
+					.username("Vandana")
+					.email("vandana@gmail.com")
+					.password("vvv")
+					.build());
+		};
 	}
 
 }

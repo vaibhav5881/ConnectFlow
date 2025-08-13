52ccc481ec55a31585d41175af43220590a544fc
Vaibhav Kumar Chaurasiya <vaibhavkumarchaurasiya5881@gmail.com>
2025-08-13 18:11:03 +0530

    Add JS files for the application logic

--- src/main/java/com/vaibhav/ConnectFlow/user/UserController.java
+++ src/main/java/com/vaibhav/ConnectFlow/user/UserController.java
@@ -18,17 +18,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public void register(User user){
+    public void register(@RequestBody User user){
         userService.register(user);
     }
 
     @PostMapping("/login")
-    public User login(User user){
+    public User login(@RequestBody User user){
         return userService.login(user);
     }
 
     @PostMapping("/logout")
-    public void logout(String email){
+    public void logout( String email){
         userService.logout(email);
     }
 
